322167b17508bace3ded455018bede1187ad1f4e
tarunmandhan <mandhantarun@gmail.com>
2022-12-19 02:29:10 +0530

    import & use clock from npm

---
+++ src/TodoList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Clock from "react-digital-clock";
+
+const TodoList = () => {
+  return (
+    <>
+      <h1>tarun</h1>
+      <Clock />;
+    </>
+  );
+};
+
+export default TodoList;
